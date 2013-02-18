48490dae9f4aa0c4d6d093ce3cb358dad82fe52b
Adrian Mejia <adriansky@gmail.com>
2013-02-17 22:02:58 -0500

    shrink and enlarge queue correctly

--- RandomizedQueuesDeques/src/RandomizedQueue.java
+++ RandomizedQueuesDeques/src/RandomizedQueue.java
@@ -21,6 +21,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     a = (Item[]) new Object[2];
     N = head = tail = 0;
   }
+
   public boolean isEmpty()           // is the queue empty?
   {
     return size() == 0;
@@ -37,9 +38,11 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     @SuppressWarnings("unchecked")
     Item[] tmp = (Item[]) new Object[capacity];
     for(int i=0; i<N; i++)
-      tmp[i] = a[i];
+      tmp[i] = a[head+i];
     a = tmp;
-    System.out.println("resize="+a.length);
+    head=0;
+    tail=N;
+//    System.out.println("resize="+a.length);
   }
   
   public void enqueue(Item item)     // add the item
@@ -52,17 +55,79 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
   public Item dequeue()              // delete and return a random item
   {
-    if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+    if (isEmpty()) 
+      throw new NoSuchElementException("Queue underflow");
+    
+    if(N==a.length/4)
+      resize(a.length/2);
+    
     N--;
     return a[head++];
   }
+  
   public Item sample()               // return (but do not delete) a random item
   {
     return null;
-    
   }
+  
   public Iterator<Item> iterator()   // return an independent iterator over items in random order
   {
-    return null;
+    return new TheIterator();
   }
+  
+  private class TheIterator implements Iterator<Item> {
+    private int current;
+    
+    public TheIterator(){
+      current = head;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#hasNext()
+     */
+    @Override
+    public boolean hasNext() {
+      return current <= tail;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#next()
+     */
+    @Override
+    public Item next() {
+      if(isEmpty()) 
+        throw new NoSuchElementException();
+      if(hasNext())
+        return a[current++];
+      return null;
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Iterator#remove()
+     */
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+    
+  }
+  
+  // FIXME: make it private before submitting homework
+  
+  public int capacity(){
+    return a.length;
+  }
+  
+  public String toStr(){
+    StringBuilder sb = new StringBuilder();
+    for(Item i : a){
+      if(i!=null){
+        sb.append(i.toString());
+        sb.append(" ");
+      }
+    }
+    return sb.toString();
+  }
+  
 }
--- RandomizedQueuesDeques/src/RandomizedQueueTest.java
+++ RandomizedQueuesDeques/src/RandomizedQueueTest.java
@@ -4,6 +4,7 @@ import static org.junit.matchers.JUnitMatchers.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import org.junit.Before;
@@ -19,77 +20,150 @@ import org.junit.Test;
  */
 public class RandomizedQueueTest {
 
-  private RandomizedQueue<String> strDeque;
+  private RandomizedQueue<String> strQueue;
   
   /**
    * @throws java.lang.Exception
    */
   @Before
   public void setUp() throws Exception {
-    strDeque = new RandomizedQueue<String>();
+    strQueue = new RandomizedQueue<String>();
   }
 
   @Test
   public void testIsEmpty() {
-    assertTrue(strDeque.isEmpty());
+    assertTrue(strQueue.isEmpty());
   }
   
   @Test
   public void testSize() {
-    assertTrue(strDeque.size() == 0);
+    assertTrue(strQueue.size() == 0);
   }
   
   @Test
   public void testIsNotEmpty() {
-    strDeque.enqueue("A");
-    assertFalse(strDeque.isEmpty());
+    strQueue.enqueue("A");
+    assertFalse(strQueue.isEmpty());
   }  
 
   @Test
   public void testSizeAndEmptyWithEnqueue() {
-    strDeque.enqueue("A");
-    strDeque.enqueue("B");
-    strDeque.enqueue("C");    
-    assertEquals(strDeque.size(), 3);
-    assertEquals(strDeque.isEmpty(), false);
+    strQueue.enqueue("A");
+    strQueue.enqueue("B");
+    strQueue.enqueue("C");    
+    assertEquals(strQueue.size(), 3);
+    assertEquals(strQueue.isEmpty(), false);
   }
   
   @Test
-  public void testSizeAndEmptyWithDequeue() {
-    strDeque.enqueue("A");
-    strDeque.enqueue("B");
-    strDeque.enqueue("C");
-    strDeque.dequeue();
-    strDeque.dequeue();
-    strDeque.dequeue();      
-    assertEquals(strDeque.size(), 0);
-    assertEquals(strDeque.isEmpty(), true);
+  public void testSizeAndEmptyWithQueueue() {
+    strQueue.enqueue("A");
+    strQueue.enqueue("B");
+    strQueue.enqueue("C");
+    strQueue.dequeue();
+    strQueue.dequeue();
+    strQueue.dequeue();      
+    assertEquals(strQueue.size(), 0);
+    assertEquals(strQueue.isEmpty(), true);
   }
   
   @Test
-  public void testEnqueueAndDequeue() {
-    strDeque.enqueue("A");
-    assertEquals("A",strDeque.dequeue());
+  public void testEnqueueAndQueueue() {
+    strQueue.enqueue("A");
+    assertEquals("A",strQueue.dequeue());
   }
   
   @Test
-  public void testEnqueueAndDequeueWithNItems() {
+  public void testEnqueueAndQueueueWithNItems() {
     String ele[] = new String[]{"A","B","C","D", "E"};
     for(String e: ele)
-      strDeque.enqueue(e);
+      strQueue.enqueue(e);
     
     ArrayList<String> arr = new ArrayList<String>();
     for(String e: ele)
       arr.add(e);
 
     for(String e : ele){
-      assertThat(arr, hasItem(strDeque.dequeue()));
+      assertThat(arr, hasItem(strQueue.dequeue()));
       arr.remove(e);
     }
-  }  
+  }
   
   @Test(expected=NoSuchElementException.class)
-  public void testEnqueueAndDequeueUnderflow() {
-    strDeque.dequeue();
+  public void testEnqueueAndQueueueUnderflow() {
+    strQueue.dequeue();
+  }
+ 
+  @Test(expected=UnsupportedOperationException.class)
+  public void testRemoveIterator() {
+    Iterator<String> it = strQueue.iterator();
+    it.remove();
+  }
+  
+  @Test(expected=NoSuchElementException.class)
+  public void testIteratorNext() {
+    Iterator<String> it = strQueue.iterator();
+    it.next();
+  }
+  
+  @Test
+  public void testIterator() {
+    String ele[] = new String[]{"A","B","C","D", "E"};
+    ArrayList<String> arr = new ArrayList<String>();
+    for(String e: ele)
+      arr.add(e);
+    
+    for(String e: ele)
+      strQueue.enqueue(e);
+    
+    Iterator<String> it = strQueue.iterator();
+    for(String e: ele){
+      assertEquals(it.hasNext(), true);
+      assertThat(arr, hasItem(it.next()));
+    }
+  }
+  
+  @Test
+  public void testIteratorToString() {
+    String ele[] = new String[]{"A","B","C","D", "E"};
+    StringBuilder sb = new StringBuilder();
+    for(String e: ele){
+      sb.append(e+" ");
+      strQueue.enqueue(e);
+    }
+    
+      assertEquals(strQueue.toStr(), sb.toString());
+  }
+  
+  @Test
+  public void testCapacityIncreaseDoubling() {
+    RandomizedQueue<Integer> intQueue = new RandomizedQueue<Integer>();
+    for(int i=0; i<100; i++){
+      intQueue.enqueue(i);
+      if(i<2) assertEquals(intQueue.capacity(), 2);
+      else{
+        int nextLg = (int) Math.pow(2,Math.ceil(Math.log(i+1)/Math.log(2)));
+//        System.out.println(String.format("i=%d; capacity=%d; nextLog=%d",i,intQueue.capacity(), nextLg));
+        assertEquals(intQueue.capacity(), nextLg);
+      }
+    }
+  }
+
+  @Test
+  public void testCapacityDecreaseHalving() {
+    RandomizedQueue<Integer> intQueue = new RandomizedQueue<Integer>();
+    
+    for(int i=0; i<100; i++)
+      intQueue.enqueue(i);
+    
+    int capacity = 128;
+    
+    for(int i=99; i>=0; i--){
+      intQueue.dequeue();
+      if(capacity/4 > intQueue.size())
+        capacity /= 2;
+      //System.out.println(String.format("i=%d; N=%d a_capacity=%d; e_capacity=%d",i,intQueue.size(), intQueue.capacity(), capacity));
+      assertEquals(intQueue.capacity(), capacity);
+    }
   }
 }
