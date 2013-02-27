b3f851f3f3114524ae5255cc2ca7e3e7746000e6
Adrian Mejia <adriansky@gmail.com>
2013-02-27 01:00:46 -0500

    implements brute

--- Collinear/src/Brute.java
+++ Collinear/src/Brute.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Brute force. Write a program Brute.java that examines 4 points at a time
  * and checks whether they all lie on the same line segment, printing out any
@@ -15,14 +17,40 @@
  * 
  */
 public class Brute {
+  
+  public ArrayList<ArrayList<Point>> collinear(Point[] points){
+    int l = points.length;
+    
+    if(l<4)
+      return null;
+    
+    ArrayList<ArrayList<Point>> res = new ArrayList<ArrayList<Point>>();
+    
+    Arrays.sort(points);
+    
+    for(int a=0; a<l; a++)
+      for(int b=a+1; b<l; b++)
+        for(int c=b+1; c<l; c++)
+          for(int d=c+1; d<l; d++){
+            double ab = points[a].slopeTo(points[b]);
+            double ac = points[a].slopeTo(points[c]);
+            double ad = points[a].slopeTo(points[d]);
+            if(ab == ac && ac == ad){
+              ArrayList<Point> p = new ArrayList<Point>();
+              p.add(points[a]);
+              p.add(points[b]);
+              p.add(points[c]);
+              p.add(points[d]);
+              res.add(p);
+            }
+          }
+    return res;
+  }
 
   /**
-   * 
-   * 
    * @param args
    */
   public static void main(String[] args) {
-    
 
   }
 
---
+++ Collinear/test/BruteTest.java
@@ -0,0 +1,101 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author adrian
+ *
+ */
+public class BruteTest {
+  private Brute p;
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    p = new Brute();
+  }
+
+  @Test
+  public void testInput1() {
+    int N = 1;
+    Point[] ps = new Point[N];
+    ps[0] = new Point(16000,16000);
+    ArrayList<ArrayList<Point>> r = p.collinear(ps);
+    assertEquals(null, r);
+  }
+  
+  @Test
+  public void testInput2() {
+    int N = 2, i=0;
+    Point[] ps = new Point[N];
+    ps[i++] = new Point(16000,16000);
+    ps[i++] = new Point(10000,16000);
+    ArrayList<ArrayList<Point>> r = p.collinear(ps);
+    assertEquals(null, r);
+  }
+  
+  @Test
+  public void testInput6() {
+    int N = 6, i=0;
+    Point[] ps = new Point[N];
+    ps[i++] = new Point(19000,10000);
+    ps[i++] = new Point(18000,10000);
+    ps[i++] = new Point(32000,10000);
+    ps[i++] = new Point(21000,10000);
+    ps[i++] = new Point(1234,5678);
+    ps[i++] = new Point(14000,10000);
+    ArrayList<ArrayList<Point>> r = p.collinear(ps);
+    
+    ArrayList<ArrayList<Point>> expected = new ArrayList<ArrayList<Point>>();
+    
+    ArrayList<Point> p;
+    
+    p = new ArrayList<Point>();
+    p.add(new Point(14000,10000));
+    p.add(new Point(18000,10000));
+    p.add(new Point(19000,10000));
+    p.add(new Point(21000,10000));
+    expected.add(p);
+    
+    p = new ArrayList<Point>();
+    p.add(new Point(14000,10000));
+    p.add(new Point(18000,10000));
+    p.add(new Point(19000,10000));
+    p.add(new Point(32000,10000));
+    expected.add(p);
+    
+    p = new ArrayList<Point>();
+    p.add(new Point(14000,10000));
+    p.add(new Point(18000,10000));
+    p.add(new Point(21000,10000));
+    p.add(new Point(32000,10000));
+    expected.add(p);
+
+    p = new ArrayList<Point>();
+    p.add(new Point(14000,10000));
+    p.add(new Point(19000,10000));
+    p.add(new Point(21000,10000));
+    p.add(new Point(32000,10000));
+    expected.add(p);
+    
+    p = new ArrayList<Point>();
+    p.add(new Point(18000,10000));
+    p.add(new Point(19000,10000));
+    p.add(new Point(21000,10000));
+    p.add(new Point(32000,10000));
+    expected.add(p);
+    
+    assertEquals(expected.size(), r.size());
+    assertEquals(expected.toString(), r.toString());
+  }
+
+}
